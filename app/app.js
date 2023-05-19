4814baf9f1336b6ee3ca6f94af91041375d1aeea
Shin Sungyun <sinsungyun1630@gmail.com>
2023-05-19 12:24:32 +0900

    프론트js 파일

--- app/app.js
+++ app/app.js
@@ -11,6 +11,7 @@ const home = require("./src/routes/home");
 // 앱 세팅
 app.set("views", "./src/views"); // views 란 폴더를 만들고 그 폴더를 views 환경(렌더링 폴더)으로 셋팅한다.
 app.set("view engine", "ejs"); // ejs 엔진으로 views 폴더 내부에 있는 html 파일을 렌더링 한다.
+app.use(express.static(`${__dirname}/src/public`)); // login html 파일을 Node.js 가 읽을 수 있게 미들웨어를 사용해 정적 경로로 진입한다. 그러면 Node.js는 public 주소까지 진입하게 되고 여기서 login.ejs의 <script src = '/js/home/login.js'>가 실행된다.
 
 app.use("/", home); // 미들 웨어 구현 메서드.
 
