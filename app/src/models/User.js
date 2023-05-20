bab5f465693bd34dfa26e1b7ea48686dc86c0ea7
Shin Sungyun <sinsungyun1630@gmail.com>
2023-05-20 12:19:01 +0900

    비동기 처리함수의 콜백 함수 반환값 undefined 확인

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -9,14 +9,14 @@ class User {
   }
   login() {
     const client = this.body;
-    const { id, password } = UserStorage.getUserInfo(client.id);
-    if (id) {
-      if (id === client.id && password === client.password) {
-        return { success: true };
-      }
-      return { success: false, msg: "비밀번호가 다릅니다" };
-    }
-    return { success: false, msg: "존재하지 않는 아이디 입니다." };
+    console.log(UserStorage.getUserInfo(client.id));
+    // if (id) {
+    //   if (id === client.id && password === client.password) {
+    //     return { success: true };
+    //   }
+    //   return { success: false, msg: "비밀번호가 다릅니다" };
+    // }
+    // return { success: false, msg: "존재하지 않는 아이디 입니다." };
   }
 
   register() {
--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,16 +1,16 @@
 "use strict";
 // 기존에 갖고있는 유저 데이터를 관리하는 UserStorage 페이지.
 
+const fs = require("fs");
 class UserStorage {
-  static #users = {
-    // 외부에서 접근 못하도록 은닉화 처리
-    id: ["shin", "pream"],
-    password: ["123", "111"],
-    name: ["페이커", "메시"],
-  };
   // 데이터를 받아올 수 있도록 메서드 만들기
   static getUsers(...field) {
-    const users = this.#users;
+    // const users = this.#users;
+    fs.readFile("./package.json", (err, data) => {
+      if (err) throw err;
+      console.log(data);
+    });
+
     const newUsers = field.reduce((newUsers, field) => {
       if (users.hasOwnProperty(field)) {
         newUsers[field] = users[field];
@@ -22,18 +22,22 @@ class UserStorage {
   }
 
   static getUserInfo(id) {
-    const users = this.#users; // {id: ["shin", "pream"] , password: ["123", "111"]}
-    const idx = users.id.indexOf(id);
-    const usersKeys = Object.keys(users);
-    const userInfo = usersKeys.reduce((newUser, info) => {
-      newUser[info] = users[info][idx];
-      return newUser;
-    }, {});
-    return userInfo;
+    // const users = this.#users; // {id: ["shin", "pream"] , password: ["123", "111"]}
+    fs.readFile("./src/databases/home/users.json", (err, data) => {
+      if (err) throw err;
+      const users = JSON.parse(data);
+      const idx = users.id.indexOf(id);
+      const usersKeys = Object.keys(users);
+      const userInfo = usersKeys.reduce((newUser, info) => {
+        newUser[info] = users[info][idx];
+        return newUser;
+      }, {});
+      return userInfo;
+    });
   }
 
   static save(userInfo) {
-    const users = this.#users;
+    // const users = this.#users;
     users.id.push(userInfo.id);
     users.name.push(userInfo.name);
     users.password.push(userInfo.password);
